9111212ce79b52a147a1f63000cc14f5f73416b5
Jorian <jorian@mail.ru>
2022-07-02 21:38:43 +0300

    flask + wsgi run with bat-file routes and 404

--- searcher/main/app.py
+++ searcher/main/app.py
@@ -1,8 +1,21 @@
 ﻿from flask import Flask
 
-app = Flask(__name__)
 
+def create_app() -> Flask:
+    app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello web!"
+    @app.route("/")
+    @app.route("/index")
+    def index(message="index"):
+        return f"{message}"
+
+    @app.route("/result")
+    def result():
+        return "result"
+
+    @app.errorhandler(404)
+    def page404(_):
+        return index("Previous page not found - 404. "
+                     "But you can search another!")
+
+    return app
