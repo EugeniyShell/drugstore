a262b335539ec620558f79af20f916b097509f0b
Jorian <jorian@mail.ru>
2022-08-23 15:06:40 +0300

    добавил постобработку результатов

--- searcher/main/decorators.py
+++ searcher/main/decorators.py
@@ -1,3 +1,6 @@
+import re
+
+
 def result_cleaner(func):
     def cleaner(*args):
         ans = func(*args)
@@ -10,8 +13,12 @@ def result_cleaner(func):
             # price = price.replace(',', '.').replace(r'[^0-9\.]', ' ')\
             #     .replace('  ', ' ').split(' ')[0]
             # price = f'от {price} рублей'
-            price = price.strip().split('.')[0].replace('₽', '') + ' ₽'
+            # price = price.split('.')[0].replace('₽', '').strip() + ' ₽'
+            price = re.search(
+                r'\d+',
+                re.sub(r'(\d+) (\d+)', r'\1\2', price)
+            ).group() + ' ₽'
             if name and price and link:
-                result.append({'name':name, 'price':price, 'link':link})
+                result.append({'name': name, 'price': price, 'link': link})
         return result
     return cleaner
